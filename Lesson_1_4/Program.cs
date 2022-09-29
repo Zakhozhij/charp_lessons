d117e046b153056db9146591c11e6c17be0fa569
Zakhozhij <maxzahojii@mail.ru>
2022-09-29 10:47:05 +0300

    added fivth folder

---
+++ Lesson_1_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Это Маша");
+}else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
